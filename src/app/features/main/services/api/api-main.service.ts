13fe134a9f8e99cd5ad8d5852a6a1a4ccacf6c9a
SrPanquesito <luisgdiazreynoso@gmail.com>
2022-11-21 19:11:55 -0800

    :art: Sticky header and filter queries to be from 2018 till date so fetching data can be faster.

--- src/app/features/main/services/api/api-main.service.ts
+++ src/app/features/main/services/api/api-main.service.ts
@@ -17,12 +17,12 @@ export class ApiMainService {
   }
 
   getAllWorksPaginated(cursor: string = '*') {
-    return this._HttpClient.get<CrossRefResponseInterface>(env.apiUrl + `/works?select=DOI,title,publisher,author,type,created,URL&rows=6&sort=published-online&order=desc&cursor=` + cursor)
+    return this._HttpClient.get<CrossRefResponseInterface>(env.apiUrl + `/works?select=DOI,title,publisher,author,type,created,URL&filter=from-pub-date:2018-01-01&rows=6&sort=published-online&order=desc&cursor=` + cursor)
     .pipe(map((res: any) => { return res }), catchError((error: any) => { console.log(error); return of(error) }))
   }
 
   getAllWorksPaginatedBySearch(query: string = '', cursor: string = '*') {
-    return this._HttpClient.get<CrossRefResponseInterface>(env.apiUrl + `/works?query=` + query + `&select=DOI,title,publisher,author,type,created,URL&rows=6&sort=relevance&order=desc&cursor=` + cursor)
+    return this._HttpClient.get<CrossRefResponseInterface>(env.apiUrl + `/works?query=` + query + `&select=DOI,title,publisher,author,type,created,URL&filter=from-pub-date:2018-01-01&rows=6&sort=relevance&order=desc&cursor=` + cursor)
     .pipe(map((res: any) => { return res }), catchError((error: any) => { console.log(error); return of(error) }))
   }
 }
