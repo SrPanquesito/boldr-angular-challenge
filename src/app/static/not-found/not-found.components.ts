9ba430a7fd1b5e35aa28ee93c39c0e63a27e5997
SrPanquesito <luisgdiazreynoso@gmail.com>
2022-11-21 19:00:57 -0800

    :art: More cosmetic changes for the search bar.

--- src/app/static/not-found/not-found.components.ts
+++ src/app/static/not-found/not-found.components.ts
@@ -3,7 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-not-found',
   template: `
-    <h1>Error 404, Page not found...</h1>
+    <div class="h-[80vh]">
+        <div class="flex justify-center items-center gap-4 h-full">
+            <span class="text-xl font-poppins font-semibold text-gray-700 dark:text-gray-200">Error 404, Page not found...</span>
+        </div>
+    </div>
   `
 })
 export class NotFoundComponent {
